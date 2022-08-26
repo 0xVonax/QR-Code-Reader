c67f5568d8bdff3c033a5b27271e613844a13cf2
0xVonax <102980368+0xVonax@users.noreply.github.com>
2022-08-26 21:00:00 +0300

    first commit

---
+++ Component/CodeScanner.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+
+export default function CodeScanner() {
+  useEffect(() => console.log("aaaaaaaaaaa effect"), []);
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
